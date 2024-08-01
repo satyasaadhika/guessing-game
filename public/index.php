5c4461b30df731fb78b8f0587b20117ed2e9245c
Priyanshu Maurya <satyasaadhika.aham@gmail.com>
2024-08-01 19:53:07 +0530

    add guessing game instruction

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/style.css">
+  <title> Priyanshu Maurya</title>
+</head>
+
 <body>
-<title> Priyanshu Maurya</title>
 <h1> Welcome to my guessing game</h1>
 
 <?php
